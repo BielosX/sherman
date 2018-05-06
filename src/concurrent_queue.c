7249417ad41e768873df280f35fa1729cab30b11
Mateusz Biel <matbiel@gmail.com>
2018-05-06 10:19:33 +0200

    add some comments

--- src/concurrent_queue.c
+++ src/concurrent_queue.c
@@ -8,9 +8,20 @@ void concurrent_queue_push(concurrent_queue_t* queue, void* item) {
     node = (concurrent_queue_node_t*)malloc(sizeof(concurrent_queue_node_t));
     node->value = item;
     node->next = NULL;
+    /* this semaphore works as a tickets machine for poping threads.
+     * Because of it it is possible to stall the thread if the queue is empty.
+     * If the size of the queue is higher than one this semaphore allows
+     * threads to pop and push concurrently withaut stalls */
     int r = sem_trywait(&queue->tickets);
     int error = errno;
+    /* This mutex prevents from concurrent head pointer modification. It may happen if two threads
+     * try to push new value at the same time */
     pthread_mutex_lock(&queue->head_mutex);
+    /* there are two possibilities for the semaphore to be in zero state:
+     * 1) queue is empty
+     * 2) poping thread has just taken the last element
+     * to prevent concurrent access to tail pointer (wich may heppen if it is taken by poping thread)
+     * tail_mutex is locked. Because it is not possible to disctinct it may be locked even if queue has not ever contained any item */
     if (r == -1 && error == EAGAIN) {
         pthread_mutex_lock(&queue->tail_mutex);
     }
@@ -33,7 +44,12 @@ void concurrent_queue_push(concurrent_queue_t* queue, void* item) {
 void* concurrent_queue_pop(concurrent_queue_t* queue) {
     concurrent_queue_node_t* node;
     void* ptr;
+    /* Wait for queue to be non-empty */
     sem_wait(&queue->tickets);
+    /* It may happen that two threads try to pop a value. Because of this mutex
+     * it is not possible to fetch the same item in two threads
+     * also, because of the semaphore, if the size of the queue is equal to 1 only one
+     * thread will take the item, the second one will stall and wait for next values */
     pthread_mutex_lock(&queue->tail_mutex);
     node = queue->tail;
     ptr = node->value;
